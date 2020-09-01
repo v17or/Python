1e821280ad45fa6af6ff78b29efd3a5ed7a38e1a
v17or <42950250+v17or@users.noreply.github.com>
2020-09-01 09:31:10 -0300

    Delete Atividade 12.py

--- Atividade 12.py
+++
@@ -1,7 +0,0 @@
-x = int(input("Digite a temperatura: "))
-y = str(input("Digite c para Celsius e f para Fahrenheit: "))
-
-if y == "c":
-    print("A temperatura em Celsius é de", x, "Cº, e em Fahrenheit é de", (9/5)*x+32, "Fº")
-if y == "f":
-    print("A temperatura em Celsius é de", (5/9)*(x-32), "Cº, e em Fahrenheit é de", x, "Fº")
